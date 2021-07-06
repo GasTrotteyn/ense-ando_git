c0aaba05283c4ecadef59180bae1427a85555276
Gaston Trotteyn <ggtrotteyn@gmail.com>
2021-07-06 16:40:15 -0300

    primer commit de esta branch, modificacion 5

--- comportamiento.js
+++ comportamiento.js
@@ -2,3 +2,5 @@ console.log('hola mundo');
 // una modificación
 // una modificación hecha en gitHub que será bajada a la pc con el comando pull
 // modificación 3
+//modificación 4
+// modificacion 5
