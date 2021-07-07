d0cbc6b3be7fa4025c25ef789dd16724d5ad38c5
Gaston Trotteyn <ggtrotteyn@gmail.com>
2021-07-07 14:38:18 -0300

    agregado contenido a comportamiento

--- comportamiento.js
+++ comportamiento.js
@@ -1,6 +1,9 @@
-console.log('hola mundo');
+console.log("hola mundo");
 // una modificación
 // una modificación hecha en gitHub que será bajada a la pc con el comando pull
 // modificación 3
 //modificación 4
 // modificacion 5
+siete;
+ocho;
+nueve;
