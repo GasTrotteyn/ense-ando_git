ed2ed089696f9e38ad2313aa37fd23685b3538a7
Gaston Trotteyn <ggtrotteyn@gmail.com>
2021-07-07 11:17:12 -0300

    traté de cambiar de branch sin commitear cambios y me hizo una adevertencia

--- carpeta_de_branch/branch.js
+++ carpeta_de_branch/branch.js
@@ -1,1 +1,2 @@
 // archivo creado para que esté solamente en la branch
+// este cambio es para ver si se guarda en el caso de que no lo comitee, pero si lo voy a pasar al staging area. Me paso a main y vuelvo para ver si se consserva
